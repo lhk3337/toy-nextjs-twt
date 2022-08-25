02f2e20e8e40fb819b91c4251a4305db8e05a3ab
lhk3337 <lhk3337@naver.com>
2022-08-25 17:54:46 +0900

    [FEAT] Login Page 생성

--- pages/log-in.tsx
+++ pages/log-in.tsx
@@ -1,3 +1,13 @@
-export default function Create() {
-  return <div>login</div>;
+import LoginModal from "../components/login";
+import { useRouter } from "next/router";
+export default function LoginPage() {
+  const router = useRouter();
+  const onClickBtn = () => {
+    router.push("/");
+  };
+  return (
+    <div className="h-screen  flex items-center justify-center  bg-slate-700">
+      <LoginModal eventClick={onClickBtn} />
+    </div>
+  );
 }
