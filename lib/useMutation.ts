d22469e8012432b227785964bbb7806cb3f982d8
lhk3337 <lhk3337@naver.com>
2022-08-26 21:31:27 +0900

    [FIX] useMutation 수정

--- lib/useMutation.ts
+++ lib/useMutation.ts
@@ -1,17 +1,18 @@
 import { useState } from "react";
+
 interface UseMutationState<T> {
   loading: boolean;
   data?: T;
   error?: object;
 }
 type UseMutationResult<T> = [(data: any) => void, UseMutationState<T>];
+
 export default function useMutation<T = any>(url: string): UseMutationResult<T> {
   const [state, setSate] = useState<UseMutationState<T>>({
     loading: false,
     data: undefined,
     error: undefined,
   });
-
   function mutation(data: any) {
     setSate((prev) => ({ ...prev, loading: true }));
     fetch(url, {
@@ -22,9 +23,8 @@ export default function useMutation<T = any>(url: string): UseMutationResult<T> 
       body: JSON.stringify(data),
     })
       .then((response) => response.json().catch(() => {}))
-      .then((data) => setSate((prev) => ({ ...prev, data })))
-      .catch((error) => setSate((prev) => ({ ...prev, error })))
-      .finally(() => setSate((prev) => ({ ...prev, loading: false })));
+      .then((data) => setSate((prev) => ({ ...prev, data, loading: false })))
+      .catch((error) => setSate((prev) => ({ ...prev, error, loading: false })));
   }
   return [mutation, { ...state }];
 }
