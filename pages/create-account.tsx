3c9a42c733bfa066a53eefa071b816898f154eec
lhk3337 <lhk3337@naver.com>
2022-08-26 21:30:19 +0900

    [FEAT] create page 생성

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -1,3 +1,13 @@
-export default function Create() {
-  return <div>create</div>;
+import { useRouter } from "next/router";
+import Create from "../components/create";
+export default function CreatePage() {
+  const router = useRouter();
+  const onClickBtn = () => {
+    router.push("/");
+  };
+  return (
+    <div className="h-screen  flex items-center justify-center  bg-slate-700">
+      <Create eventClick={onClickBtn} />
+    </div>
+  );
 }
