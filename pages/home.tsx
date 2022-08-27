6839bc054b4ac3294fbd43568677a9a45f506340
lhk3337 <lhk3337@naver.com>
2022-08-27 23:51:46 +0900

    [FEAT] 사용자 로그인 후 메인에 사용자 이메일 표시하기

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,3 +1,22 @@
+import useMutation from "../lib/useMutation";
+import { useForm } from "react-hook-form";
+import { register } from "ts-node";
+import useSWR from "swr";
+// interface MutationResult {
+//   ok: boolean;
+//   exists: any;
+// }
+// interface TokenForm {
+//   token: string;
+// }
 export default function Home() {
-  return "home";
+  // const { register, handleSubmit: tokenHandleSubmit } = useForm<TokenForm>();
+  // const [confirmToken, { loading: tokenLoading, data: UserData }] = useMutation<MutationResult>("/api/me");
+  const { data, error } = useSWR("api/me");
+
+  return (
+    <>
+      <h1>{data?.user.email}</h1>
+    </>
+  );
 }
