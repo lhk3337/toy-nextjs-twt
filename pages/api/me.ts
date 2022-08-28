3092553caccbc384cf3914134c2b6de268927f6b
lhk3337 <lhk3337@naver.com>
2022-08-28 21:51:48 +0900

    [FIX]

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -1,13 +1,12 @@
 import { withApiSession } from "../../lib/withSession";
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "../../lib/withHandler";
+
 import db from "../../lib/db";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!req.session.user) return res.status(400).json({ ok: false });
   const user = await db.user.findUnique({ where: { email: req.session.user?.email } });
 
-  //   res.status(200).end();
   return res.json({
     ok: true,
     user,
