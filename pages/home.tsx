9383b62d41701aa27575d3d90cee56c88a2612f8
lhk3337 <lhk3337@naver.com>
2022-08-28 21:52:26 +0900

    [FIX] home 페이지에 react hook form 추가

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,22 +1,28 @@
 import useMutation from "../lib/useMutation";
-import { useForm } from "react-hook-form";
-import { register } from "ts-node";
+import { useForm, SubmitHandler } from "react-hook-form";
+
 import useSWR from "swr";
-// interface MutationResult {
-//   ok: boolean;
-//   exists: any;
-// }
-// interface TokenForm {
-//   token: string;
-// }
+interface IUser {
+  tweets: string;
+}
 export default function Home() {
-  // const { register, handleSubmit: tokenHandleSubmit } = useForm<TokenForm>();
+  const onSubmit: SubmitHandler<IUser> = (data) => {
+    console.log(data);
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IUser>();
   // const [confirmToken, { loading: tokenLoading, data: UserData }] = useMutation<MutationResult>("/api/me");
   const { data, error } = useSWR("api/me");
 
   return (
     <>
       <h1>{data?.user.email}</h1>
+      <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register("tweets", { required: "이메일 주소를 입력해주세요" })} />
+      </form>
     </>
   );
 }
