bcb591efb733630ee82683ce51894bf330e885fa
lhk3337 <lhk3337@naver.com>
2022-08-30 00:40:57 +0900

    [FEAT] comment HOF

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -14,4 +14,35 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withHandler("POST", handler); // HOF(고차 함수)
+
+/*
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcryptjs";
+import db from "../../lib/db";
+
+export default function withHandler() {
+  return async function (req: NextApiRequest, res: NextApiResponse): Promise<any> {
+    async function handler(req: NextApiRequest, res: NextApiResponse) {
+      const { userId, password } = req.body;
+      await db.user.create({
+        data: {
+          userId,
+          password: bcrypt.hashSync(password, 10),
+        },
+      });
+
+      res.status(200).end();
+    }
+    if (req.method !== "POST") {
+      return res.status(405).end();
+    }
+    try {
+      await handler(req, res);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error });
+    }
+  };
+}
+*/
