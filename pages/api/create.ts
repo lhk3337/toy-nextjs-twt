3f0bbc4ff44867bfb2a5a4bfc84676c253784498
lhk3337 <lhk3337@naver.com>
2022-08-31 19:17:42 +0900

    [FEAT create api  error message

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -3,15 +3,24 @@ import withHandler from "../../lib/withHandler";
 import bcrypt from "bcryptjs";
 import db from "../../lib/db";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { userId, password } = req.body;
-  await db.user.create({
-    data: {
-      userId,
-      password: bcrypt.hashSync(password, 10),
-    },
-  });
+  const {
+    body: { userId, password },
+  } = req;
 
-  res.status(200).end();
+  const user = await db.user.findUnique({ where: { userId } }); // 유저 확인 유무
+
+  //유저가 없으면 db에 user정보를 생성 하고 아닐경우 에러 메시지 생성
+  if (!user) {
+    res.json({ create: true });
+    await db.user.create({
+      data: {
+        userId,
+        password: bcrypt.hashSync(password, 10),
+      },
+    });
+  } else {
+    return res.json({ create: false, message: "이미 가입된 아이디 입니다. 다른 아이디를 사용해 주세요" });
+  }
 }
 
 export default withHandler("POST", handler); // HOF(고차 함수)
