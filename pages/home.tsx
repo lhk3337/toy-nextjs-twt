9e62aa02a9db503d443cd00062c751a97069a824
lhk3337 <lhk3337@naver.com>
2022-09-04 18:41:08 +0900

    [FEAT] home 트윗부분 style 설정하기

--- pages/home.tsx
+++ pages/home.tsx
@@ -8,14 +8,14 @@ interface IUser {
   tweets: string;
 }
 export default function Home() {
-  // const onSubmit: SubmitHandler<IUser> = (data) => {
-  //   console.log(data);
-  // };
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  // } = useForm<IUser>();
+  const onSubmit: SubmitHandler<IUser> = (data) => {
+    console.log(data);
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IUser>();
   // const [confirmToken, { loading: tokenLoading, data: UserData }] = useMutation<MutationResult>("/api/me");
   const router = useRouter();
   const { data, error } = useSWR("api/me");
@@ -27,15 +27,37 @@ export default function Home() {
     }
   }, [router, error]);
   return (
-    <>
-      <h1>어서오세요 {data?.user.userId}님</h1>
-      <Link href={`/tweet/${data?.user.id}`}>
-        <a>
-          <div className="w-[300px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            이동하기
+    <div className="bg-black  max-w-2xl mx-auto border-[#2f3336] border-x-2 text-white py-4">
+      <h1 className="text-white mt-5 ml-8 font-bold text-lg">{data?.user.userId}</h1>
+      <div className="flex justify-end m-4">
+        <div>
+          <input
+            type="text"
+            placeholder="무슨 일이 일어나고 있나요?"
+            {...register}
+            className="w-[500px] text-2xl m-4 mr-0 h-10 focus:outline-none bg-transparent border-b-2 border-[#2f3336] placeholder:text-[#2f3336] placeholder:font-bold placeholder:text-2xl"
+          />
+          <div className=" text-white ml-[440px] my-2 py-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-3xl text-sm  text-center dark:bg-blue-500 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer">
+            <button>트윗하기</button>
           </div>
-        </a>
-      </Link>
-    </>
+        </div>
+      </div>
+      <div className="border-b-2 border-[#2f3336]" />
+      <div className="mt-2 p-4">
+        {Array(18)
+          .fill("")
+          .map(() => {
+            return (
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste atque omnis quia quae vitae accusamus
+                reiciendis fuga deserunt tempore consectetur veniam voluptas at rerum vel illum, reprehenderit maiores
+                culpa possimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ea molestiae, ad quia
+                assumenda nesciunt quos architecto deserunt magni nihil laudantium quae eveniet totam ab perspiciatis
+                tenetur facilis ducimus dicta.
+              </p>
+            );
+          })}
+      </div>
+    </div>
   );
 }
