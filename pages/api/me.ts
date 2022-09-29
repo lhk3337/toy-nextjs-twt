88bc3582f3e2039542ae49d6c93e203db28a4cba
lhk3337 <lhk3337@naver.com>
2022-09-29 18:07:36 +0900

    [FIX] profile api 항목 필요한 부분만 가져오기

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -2,14 +2,18 @@ import { withApiSession } from "../../lib/withSession";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import db from "../../lib/db";
+import withHandler from "../../lib/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!req.session.user) return res.status(400).json({ ok: false });
-  const user = await db.user.findUnique({ where: { userId: req.session.user?.userId } });
+  const user = await db.user.findUnique({
+    where: { userId: req.session.user?.userId },
+    select: { userId: true, id: true },
+  });
 
   return res.json({
     ok: true,
     user,
   });
 }
-export default withApiSession(handler);
+export default withApiSession(withHandler("GET", handler));
