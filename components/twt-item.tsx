2d460769b5520d48246e2c14f18ac7c2c7448684
lhk3337 <lhk3337@naver.com>
2022-11-30 13:24:49 +0900

    [FEAT] profile main page 스타일 ,해당 트위터 디테일 페이지 링크 설정

--- components/twt-item.tsx
+++ components/twt-item.tsx
@@ -1,64 +1,63 @@
 import { TwtListItem } from "@components/twt-list";
+import Link from "next/link";
 
-export default function TwtItem({ name, text, date }: TwtListItem) {
+export default function TwtItem({ id, name, text, date }: TwtListItem) {
   return (
     <>
-      <div className="p-3 flex flex-col">
-        <span className="text-gray-500 text-sm text-right">{date}</span>
-        <div className="px-2 flex">
-          <div className="mr-6">
-            <div className="h-12 w-12 rounded-full bg-slate-300 p-4" />
-          </div>
-          <div className="flex flex-col">
-            <span className="text-lg font-semibold mb-2">{name}</span>
-            <span className="text-sm">{text}</span>
-            <div className="p-4 flex space-x-20 ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 text-[#94A3B8]"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
-                />
-              </svg>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 text-[#94A3B8]"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={1.5}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 text-[#94A3B8]"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={1.5}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
-                />
-              </svg>
+      <Link href={`/tweet/${id}`}>
+        <a className="p-3 flex flex-col">
+          <span className="text-gray-500 text-sm text-right">{date}</span>
+          <div className="px-2 flex">
+            <div className="mr-6">
+              <div className="h-12 w-12 rounded-full bg-slate-300 p-4" />
+            </div>
+            <div className="flex flex-col">
+              <span className="text-lg font-semibold mb-2">{name}</span>
+              <span className="text-sm">{text}</span>
             </div>
           </div>
-        </div>
+        </a>
+      </Link>
+      <div className="p-4 pl-24 flex space-x-20 ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 text-[#94A3B8]"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6 text-[#94A3B8]"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={1.5}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6 text-[#94A3B8]"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={1.5}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+        </svg>
       </div>
       <div className="border-b-2 border-[#2f3336]" />
     </>
---
+++ pages/profile/index.tsx
@@ -0,0 +1,80 @@
+import Layout from "@components/layout";
+import TwtList from "@components/twt-list";
+import { cls } from "lib/util";
+import { useRouter } from "next/router";
+import { useState } from "react";
+export default function Profile() {
+  const router = useRouter();
+  const onMoveClick = () => {
+    router.push("/profile/edit");
+  };
+  const [method, setMethod] = useState<"twt" | "likes">("twt");
+  const onTwtListClick = () => {
+    setMethod("twt");
+  };
+  const onLikesListClick = () => {
+    setMethod("likes");
+  };
+
+  const twtFakeData = [
+    { id: 1, name: "holim", date: "22.11.25. 오후 4:31", text: "hello world" },
+    { id: 2, name: "martin", date: "22.11.20. 오후 4:31", text: "before and after being denied dino" },
+    { id: 3, name: "katie", date: "22.10.02. 오전 4:31", text: "A Few Humble Cinema Lovers" },
+    { id: 4, name: "Saltydkdan", date: "22.12.25. 오후 4:31", text: "I need for shiny charm" },
+    { id: 5, name: "Cinema Tweets", date: "22.9.25. 오후 4:21", text: "god bless you" },
+    { id: 6, name: "Tortie", date: "22.4.25. 오후 1:34", text: "Am I doing this challenge right?" },
+  ];
+
+  const likesFakeData = [
+    { id: 1, name: "martin", date: "22.11.20. 오후 4:31", text: "before and after being denied dino" },
+    { id: 2, name: "Cinema Tweets", date: "22.9.25. 오후 4:21", text: "god bless you" },
+    { id: 3, name: "Tortie", date: "22.4.25. 오후 1:34", text: "Am I doing this challenge right?" },
+  ];
+
+  return (
+    <Layout title="Profile" canGoBack>
+      <div className="px-10 py-10 flex mb-3 items-center justify-between">
+        <div className="flex items-center">
+          <div className="h-24 w-24 rounded-full bg-slate-300 p-12" />
+          <span className="ml-10 text-2xl">holim</span>
+        </div>
+        <button
+          onClick={onMoveClick}
+          className="rounded-3xl border border-white px-4 py-2 hover:bg-[#eff3f41a] hover:transition hover:duration-300"
+        >
+          Edit Profile
+        </button>
+      </div>
+      <div className="flex justify-center">
+        <div className="grid w-full grid-cols-2 border-b  border-[#2f3336]">
+          <button
+            className={cls(
+              "pb-2 transition duration-500",
+              method === "twt"
+                ? "font-bold border-b-4 border-b-[#1d9bf0] text-white"
+                : "border-transparent text-gray-500"
+            )}
+            onClick={onTwtListClick}
+          >
+            Tweets
+          </button>
+          <button
+            className={cls(
+              "pb-2 transition duration-500 ",
+              method === "likes"
+                ? "border-b-4 border-b-[#1d9bf0] text-white font-bold"
+                : "border-transparent text-gray-500"
+            )}
+            onClick={onLikesListClick}
+          >
+            Likes
+          </button>
+        </div>
+      </div>
+      <div className="my-2 p-3">
+        {method === "twt" && <TwtList data={twtFakeData} />}
+        {method === "likes" && <TwtList data={likesFakeData} />}
+      </div>
+    </Layout>
+  );
+}
