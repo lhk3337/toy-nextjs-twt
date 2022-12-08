e61dcf392550a9c4aac563922335ac77fc4236c8
lhk3337 <lhk3337@naver.com>
2022-12-08 11:01:25 +0900

    [FEAT] tweet 생성 및 리스트 api

---
+++ pages/api/tweets/index.ts
@@ -0,0 +1,26 @@
+import db from "@libs/server/db";
+import withHandler from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    const tweetList = await db.tweets.findMany({
+      include: { user: { select: { userId: true } } },
+    });
+    res.json({ ok: true, tweetList });
+  }
+  if (req.method === "POST") {
+    const {
+      body: { tweets },
+      session: { user },
+    } = req;
+    const tweetWrite = await db.tweets.create({
+      data: {
+        user: { connect: { userId: user?.userId } },
+        tweet: tweets,
+      },
+    });
+    res.json({ ok: true });
+  }
+}
+export default withApiSession(withHandler({ methods: ["GET", "POST"], handler }));
