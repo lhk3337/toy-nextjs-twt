08c1c644c586189ec0aeeab74bba383737624471
lhk3337 <lhk3337@naver.com>
2022-12-17 13:01:51 +0900

    [FEAT] tweet detail - 해당 tweet api 가져오기

--- pages/tweet/[id].tsx
+++ pages/tweet/[id].tsx
@@ -1,13 +1,17 @@
 import Button from "@components/button";
 import Layout from "@components/layout";
 import TwtItem from "@components/twt-item";
+import { TwtListItem } from "@components/twt-list";
+import { Tweets } from "@prisma/client";
+import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
+import useSWR from "swr";
 interface rtweetForm {
   rtweet: string;
 }
 
-export default function Post() {
+const TweetDetail: NextPage = () => {
   const {
     register,
     handleSubmit,
@@ -19,68 +23,82 @@ export default function Post() {
     console.log(data);
     reset();
   };
-  const fakeData = { id: 1, name: "holim", text: "hello", date: "22.11.25. 오후 4:31" };
+  const router = useRouter();
+  const { data } = useSWR(router.query.id ? `/api/tweets/${router.query.id}` : null);
+
+  // const fakeData = { id: 1, name: "holim", text: "hello", updatedAt: new Date() };
   return (
     <Layout title="Detail Tweet" canGoBack>
-      <TwtItem {...fakeData} />
+      {!data ? <div className=" h-[10vh] bg-gray-600 mx-5 rounded-md animate-pulse" /> : <TwtItem {...data?.tweet} />}
       <div className="p-5 pt-8 border-b-2 border-[#2f3336]">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex">
-            <div className="mr-5">
-              <div className="h-12 w-12 rounded-full bg-slate-300 p-4" />
+        {!data ? (
+          <div className=" h-[15vh] bg-gray-600  rounded-md animate-pulse" />
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex">
+              <div className="mr-5">
+                <div className="h-12 w-12 rounded-full bg-slate-300 p-4" />
+              </div>
+              <textarea
+                placeholder="트윗 답장 하기"
+                rows={3}
+                {...register("rtweet")}
+                className="text-xl resize-none w-full scrollbar-hide focus:outline-none bg-transparent placeholder:text-[#2f3336] placeholder:font-semibold placeholder:text-xl"
+              />
             </div>
-            <textarea
-              placeholder="트윗 답장 하기"
-              rows={3}
-              {...register("rtweet")}
-              className="text-xl resize-none w-full scrollbar-hide focus:outline-none bg-transparent placeholder:text-[#2f3336] placeholder:font-semibold placeholder:text-xl"
-            />
-          </div>
-          <div className="flex justify-end">
-            <Button btnName="Reply" />
-          </div>
-        </form>
+            <div className="flex justify-end">
+              <Button btnName="Reply" />
+            </div>
+          </form>
+        )}
       </div>
       {Array(10)
         .fill("")
         .map((_, i) => (
           <div key={i} className="p-5 border-b-2 border-[#2f3336] relative ">
-            <div className="absolute right-4 justify-center items-center flex h-10 w-10 hover:rounded-full hover:bg-[#eff3f41a] hover:transition hover:duration-300">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 cursor-pointer"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                />
-              </svg>
-            </div>
-            <div className="flex flex-col">
-              <div className="flex">
-                <div className="mr-6">
-                  <div className="h-12 w-12 rounded-full bg-slate-300 p-4" />
+            {!data ? (
+              <div className=" h-[10vh] bg-gray-600  rounded-md animate-pulse" />
+            ) : (
+              <div>
+                <div className="absolute right-4 justify-center items-center flex h-10 w-10 hover:rounded-full hover:bg-[#eff3f41a] hover:transition hover:duration-300">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 cursor-pointer"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                    />
+                  </svg>
                 </div>
                 <div className="flex flex-col">
-                  <div className="flex flex-col mb-3">
-                    <span className="text-lg font-semibold">name</span>
-                    <span className="text-gray-500 text-sm">22.11.25. 오후 4:31</span>
+                  <div className="flex">
+                    <div className="mr-6">
+                      <div className="h-12 w-12 rounded-full bg-slate-300 p-4" />
+                    </div>
+                    <div className="flex flex-col">
+                      <div className="flex flex-col mb-3">
+                        <span className="text-lg font-semibold">name</span>
+                        <span className="text-gray-500 text-sm">22.11.25. 오후 4:31</span>
+                      </div>
+                      <span className="text-sm">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores voluptatibus mollitia, quo
+                        odit praesentium aspernatur quam veritatis. Perspiciatis, quos explicabo distinctio, totam iusto
+                        autem tenetur veritatis dolore, unde laudantium nesciunt.
+                      </span>
+                    </div>
                   </div>
-                  <span className="text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores voluptatibus mollitia, quo odit
-                    praesentium aspernatur quam veritatis. Perspiciatis, quos explicabo distinctio, totam iusto autem
-                    tenetur veritatis dolore, unde laudantium nesciunt.
-                  </span>
                 </div>
               </div>
-            </div>
+            )}
           </div>
         ))}
     </Layout>
   );
-}
+};
+export default TweetDetail;
