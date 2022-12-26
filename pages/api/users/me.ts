3b8d52acaa7d528f3e5210279be8cb6521003b6d
lhk3337 <lhk3337@naver.com>
2022-12-26 18:25:19 +0900

    [FEAT] Profile Info api update 설정하기

--- pages/api/users/me.ts
+++ pages/api/users/me.ts
@@ -5,19 +5,82 @@ import db from "@libs/server/db";
 import withHandler from "@libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const {
-    session: { user },
-  } = req;
+  if (req.method === "GET") {
+    const {
+      session: { user },
+    } = req;
+    if (!user) return res.status(400).json({ ok: false });
+    const users = await db.user.findUnique({
+      where: { id: user?.id },
+      select: { userId: true, id: true, name: true, bio: true, location: true, website: true, createdAt: true },
+    });
 
-  if (!user) return res.status(400).json({ ok: false });
-  const users = await db.user.findUnique({
-    where: { id: user?.id },
-    select: { userId: true, id: true, name: true },
-  });
+    return res.json({
+      ok: true,
+      user: users,
+    });
+  }
+  if (req.method === "POST") {
+    const {
+      session: { user },
+      body: { bio, website, name, location },
+    } = req;
 
-  return res.json({
-    ok: true,
-    user: users,
-  });
+    if (bio) {
+      await db.user.update({
+        where: { id: user?.id },
+        data: {
+          bio,
+        },
+      });
+    } else {
+      await db.user.update({
+        where: { id: user?.id },
+        data: { bio: "" },
+      });
+    }
+
+    if (website) {
+      await db.user.update({
+        where: { id: user?.id },
+        data: {
+          website,
+        },
+      });
+    } else {
+      await db.user.update({
+        where: { id: user?.id },
+        data: { website: "" },
+      });
+    }
+    if (name) {
+      await db.user.update({
+        where: { id: user?.id },
+        data: {
+          name,
+        },
+      });
+    } else {
+      await db.user.update({
+        where: { id: user?.id },
+        data: { name: "Anonymous" },
+      });
+    }
+    if (location) {
+      await db.user.update({
+        where: { id: user?.id },
+        data: {
+          location,
+        },
+      });
+    } else {
+      await db.user.update({
+        where: { id: user?.id },
+        data: { location: "" },
+      });
+    }
+
+    res.json({ ok: true });
+  }
 }
-export default withApiSession(withHandler({ methods: ["GET"], handler }));
+export default withApiSession(withHandler({ methods: ["GET", "POST"], handler }));
