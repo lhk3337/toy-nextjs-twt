db16a50ee4cb99656a21a815b32013e7507e579a
lhk3337 <lhk3337@naver.com>
2023-01-07 17:58:16 +0900

    [FEAT] add Favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { SWRConfig } from "swr";
 import "../global.css";
+import favicon from "../public/favicon.ico";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
         fetcher: (url: string) => fetch(url).then((response) => response.json()),
       }}
     >
+      <Head>
+        <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
+      </Head>
       <Component {...pageProps} />
     </SWRConfig>
   );
