8133bb09fe7bf51f113ada087a7e745128ac0fe3
lhk3337 <lhk3337@naver.com>
2023-01-10 10:47:29 +0900

    [FEAT] message list page

--- pages/message/index.tsx
+++ pages/message/index.tsx
@@ -19,33 +19,39 @@ export default function Message() {
   const { user } = useUser();
   return (
     <Layout title="Message">
-      <div className="mt-3">
-        {data?.chatList.map((chat: ChatDetail) => {
-          return (
-            <Link key={chat.id} href={`/message/${chat.id}`}>
-              <a className="p-5 flex hover:bg-[#16181c]">
-                <div className="mr-6">
-                  <div className="h-12 w-12 rounded-full bg-slate-300 p-4" />
-                </div>
-                <div>
-                  <div className="flex items-center">
-                    <span className="text-lg font-semibold">
-                      {user?.id === chat.senderId
-                        ? chat.receiver.userId
-                        : user?.id === chat.receiverId && chat.sender.userId}
-                    </span>
-                    <span className="mx-2 text-gray-500">・</span>
-                    <span className="text-gray-500 text-sm">
-                      <Time time={new Date(chat.createdAt)} />
-                    </span>
+      {!data ? (
+        <div className="p-5 h-screen bg-slate-500 mx-5 my-7  rounded-md animate-pulse" />
+      ) : (
+        <div className="mt-3">
+          {data?.chatList.map((chat: ChatDetail) => {
+            return (
+              <Link key={chat.id} href={`/message/${chat.id}`}>
+                <a className="p-5 flex hover:bg-[#16181c]">
+                  <div className="mr-6">
+                    <div className="h-12 w-12 rounded-full bg-slate-300 p-4" />
                   </div>
-                  <div>{chat.msgs[chat.msgs.length - 1]?.message}</div>
-                </div>
-              </a>
-            </Link>
-          );
-        })}
-      </div>
+                  <div>
+                    <div className="flex items-center">
+                      <span className="text-lg font-semibold">
+                        {user?.id === chat.senderId
+                          ? chat.receiver.userId
+                          : user?.id === chat.receiverId && chat.sender.userId}
+                      </span>
+                      <span className="mx-2 text-slate-500">・</span>
+                      <Time time={new Date(chat.createdAt)} />
+                    </div>
+                    <div className="text-gray-400">
+                      {chat.msgs[chat.msgs.length - 1]?.message.length < 40
+                        ? chat.msgs[chat.msgs.length - 1]?.message
+                        : `${chat.msgs[chat.msgs.length - 1]?.message.substring(0, 40)} ...`}
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </Layout>
   );
 }
