21fc369f40fa64072bfd19f9dcb363cdf45ffb9a
lhk3337 <lhk3337@naver.com>
2023-01-23 14:29:35 +0900

    [FEAT] bookmark infinit scroll

--- pages/api/bookmark/index.ts
+++ pages/api/bookmark/index.ts
@@ -5,9 +5,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const {
     session: { user },
+    query: { page },
   } = req;
 
   const tweetLikeList = await db.bookmarks.findMany({
+    take: 10,
+    skip: (Number(page) - 1) * 10,
     where: { userId: user?.id },
     orderBy: [{ id: "desc" }],
     include: {
--- pages/bookmark/index.tsx
+++ pages/bookmark/index.tsx
@@ -1,10 +1,16 @@
 import Layout from "@components/layout";
 import TwtList from "@components/twt-list";
 import usePagination from "@libs/client/usePagination";
-import useSWR from "swr";
+import { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 export default function Bookmarks() {
-  const { data } = useSWR("/api/bookmark");
+  // const { data } = useSWR("/api/bookmark");
   const { mutate } = usePagination("/api/tweets/");
+  const { data, isReachedEnd, setSize, page } = usePagination(`/api/bookmark`);
+  useEffect(() => {
+    setSize(page);
+  }, [setSize, page]);
   return (
     <Layout title="Bookmark" mutate={mutate}>
       {!data ? (
@@ -14,7 +20,20 @@ export default function Bookmarks() {
           })}
         </>
       ) : (
-        <TwtList twtList={data?.tweetList} />
+        <InfiniteScroll
+          next={() => setSize(page)}
+          dataLength={data?.length ?? 0}
+          hasMore={!isReachedEnd}
+          loader={
+            <div className="flex justify-center py-5">
+              <div className="border-b-transparent border-solid animate-spin rounded-full border-blue-400 border-4 h-8 w-8" />
+            </div>
+          }
+        >
+          {data?.map((value, i) => (
+            <TwtList twtList={value.tweetList} key={i} />
+          ))}
+        </InfiniteScroll>
       )}
     </Layout>
   );
